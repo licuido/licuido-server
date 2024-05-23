20e5e7de7ea3e9d134339f99ad45261dbb5c4845
prem-svg <premm2201@gmail.com>
2024-05-23 16:21:29 +0530

    removed token view validation on investor

--- src/api/v1/private/token_offering/handlers/get.ts
+++ src/api/v1/private/token_offering/handlers/get.ts
@@ -11,20 +11,6 @@ export async function FIND_TOKEN(request: FastifyRequest, reply: FastifyReply) {
     const { entity_id, user_entity_id, user_profile_id, ...rest } =
       queryRequestInfo(request);
 
-    console.log(user_entity_id, "user_entity_id");
-
-    if (entity_id === 2) {
-      return handleResponse(
-        request,
-        reply,
-        responseType?.INTERNAL_SERVER_ERROR,
-        {
-          error: {
-            message: "Only Issuer and admin can be view token offering",
-          },
-        }
-      );
-    }
 
     // -----------------------------
     //  INTERACTOR
