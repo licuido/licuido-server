7dc8575052fe2692c4b65d112c42b301900dd6bf
kumaravelpazhani <kumaravel-crayond@caratlane.com>
2024-08-20 22:24:03 +0530

    currency conversion issue fixed

--- src/api/v1/private/currency_conversion/handlers/get.ts
+++ src/api/v1/private/currency_conversion/handlers/get.ts
@@ -3,9 +3,10 @@ import {
   Logger,
   handleResponse,
   responseType,
-  currencyConvert,
+  // currencyConvert,
+  makeNetworkRequest,
 } from "@helpers";
-// import { isOneHourCompleted, cache } from "@utils";
+import { isOneHourCompleted, cache } from "@utils";
 import { queryRequestInfo } from "@mappers";
 
 // Get Investor Count for Qualification
@@ -18,96 +19,77 @@ export async function CURRENCY_CONVERSION_HANDLER(
     const { from, amount, to } = queryRequestInfo(request);
     console.log(from);
     console.log(to);
-    const cc = await currencyConvert({
-      from_currency_code: from,
-      to_currency_code: to,
-      amount: 1,
-    });
+    // const cc = await currencyConvert({
+    //   from_currency_code: from,
+    //   to_currency_code: to,
+    //   amount: 1,
+    // });
     // const cc = await currencyConvert({ from_currency_code: from, to_currency_code: to, amount: Number(amount) ?? 0 })
 
-    // if (!amount) {
-    //   return handleResponse(request, reply, responseType?.BAD_REQUEST, {
-    //     error: {
-    //       message: "Amount required",
-    //     },
-    //   });
-    // }
-    // const cachedData = cache.get("USD");
-    // let returnAmount = 0;
-    // if (cachedData) {
-    //   console.log("cached");
-    //   const isproceed: any = isOneHourCompleted(cachedData.timestamp);
-    //   if (!isproceed) {
-    //     console.log("procced");
-    //     let toCurrency = cachedData.rates["" + to];
-    //     returnAmount = toCurrency * parseInt(amount);
-    //   } else {
-    //     console.log("noprocced");
-    //     const conversionResponse: any = {
-    //       disclaimer:
-    //         "Usage subject to terms: https://openexchangerates.org/terms",
-    //       license: "https://openexchangerates.org/license",
-    //       timestamp: 1723176001,
-    //       base: "USD",
-    //       rates: {
-    //         AED: 3.673,
-    //         AFN: 70.866359,
-    //         ALL: 91.767072,
-    //         AMD: 388.428642,
-    //         ANG: 1.804187,
-    //         AOA: 879.690333,
-    //         EUR: 0.915485,
-    //         GBP: 0.783789,
-    //         USD: 1,
-    //       },
-    //     };
-    //     let toCurrency = conversionResponse.rates["" + to];
-    //     returnAmount = toCurrency * parseInt(amount);
-    //     cache.set("USD", conversionResponse);
-    //   }
-    // } else {
-    //   console.log("nocached");
-    //   const conversionResponse: any = {
-    //     disclaimer:
-    //       "Usage subject to terms: https://openexchangerates.org/terms",
-    //     license: "https://openexchangerates.org/license",
-    //     timestamp: 1723176001,
-    //     base: "USD",
-    //     rates: {
-    //       AED: 3.673,
-    //       AFN: 70.866359,
-    //       ALL: 91.767072,
-    //       AMD: 388.428642,
-    //       ANG: 1.804187,
-    //       AOA: 879.690333,
-    //       EUR: 0.915485,
-    //       GBP: 0.783789,
-    //       USD: 1,
-    //     },
-    //   };
-    //   let toCurrency = conversionResponse.rates["" + to];
-    //   returnAmount = toCurrency * parseInt(amount);
-    //   cache.set("USD", conversionResponse);
-    // }
-
-    // const conversionResponse = await makeNetworkRequest(
-    //     {
-    //         url: "https://openexchangerates.org/api/latest.json?app_id=84afe04c2c534b13ba74cefc2c88db40&symbols=EUR,GBP,USD,AAD,AED,AFN,ALL,AMD,ANG,AOA",
-    //         config: {
-    //             method: "GET",
-    //         },
-    //     }
-    // );
-
-    // /* -----------  INTERACTOR ----------- */
-    // const result = await UserEntities.getInvestorCountForQualification({
-    //     entity_type_id: 2, // Investor
-    // });
-
-    /* -----------  RESPONSE ----------- */
+    if (!amount) {
+      return handleResponse(request, reply, responseType?.BAD_REQUEST, {
+        error: {
+          message: "Amount required",
+        },
+      });
+    }
+    const cachedData = cache.get("USD");
+    let returnAmount = 0;
+    if (cachedData) {
+      console.log("cached");
+      const isproceed: any = isOneHourCompleted(cachedData.timestamp);
+      if (!isproceed) {
+        console.log("procced");
+        let toCurrency = cachedData.rates["" + to];
+        returnAmount = toCurrency;
+        // returnAmount = toCurrency * parseInt(amount);
+      } else {
+        console.log("noprocced");
+        const conversionResponse: any = await makeNetworkRequest({
+          url: "https://api.exchangeratesapi.io/v1/latest?access_key=44ed019d4c73ecdc6117b5e9ef36c57c&symbols=EUR,GBP,USD,AAD,AED,AFN,ALL,AMD,ANG,AOA&format=1",
+          config: {
+            method: "GET",
+          },
+        });
+        let toCurrency = conversionResponse.rates["" + to];
+        returnAmount = toCurrency;
+        // returnAmount = toCurrency * parseInt(amount);
+        cache.set("USD", conversionResponse);
+      }
+    } else {
+      // console.log("nocached");
+      // const conversionResponse: any = {
+      //   disclaimer:
+      //     "Usage subject to terms: https://openexchangerates.org/terms",
+      //   license: "https://openexchangerates.org/license",
+      //   timestamp: 1723176001,
+      //   base: "USD",
+      //   rates: {
+      //     AED: 3.673,
+      //     AFN: 70.866359,
+      //     ALL: 91.767072,
+      //     AMD: 388.428642,
+      //     ANG: 1.804187,
+      //     AOA: 879.690333,
+      //     EUR: 0.915485,
+      //     GBP: 0.783789,
+      //     USD: 1,
+      //   },
+      // };
+      const conversionResponse: any = await makeNetworkRequest({
+        url: "https://api.exchangeratesapi.io/v1/latest?access_key=44ed019d4c73ecdc6117b5e9ef36c57c&symbols=EUR,GBP,USD,AAD,AED,AFN,ALL,AMD,ANG,AOA&format=1",
+        config: {
+          method: "GET",
+        },
+      });
+      let toCurrency = conversionResponse.rates["" + to];
+      returnAmount = toCurrency;
+      // returnAmount = toCurrency * parseInt(amount);
+      cache.set("USD", conversionResponse);
+    }
 
     return handleResponse(request, reply, responseType?.OK, {
-      data: { from, from_amount: amount, to, to_amount: cc },
+      data: { from, from_amount: amount, to, to_amount: returnAmount },
     });
   } catch (error: any) {
     Logger.error(request, error.message, error);
